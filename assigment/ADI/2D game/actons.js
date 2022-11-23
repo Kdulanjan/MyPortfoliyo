ca82c29a75586a0b1a75c665847566f8d66ae798
Kdulanjan <kalananil19@gmail.com>
2022-11-23 16:58:07 +0530

    create 2d game movement

---
+++ assigment/ADI/2D game/actons.js
@@ -0,0 +1,53 @@
+
+// -----------------------------Breath Animation----------------------------
+var Robo= document.getElementById("Robo");
+CharecterImageNumber = 0 ;
+AnimationNumber = 0 ;
+
+function MoveAnimation(){
+    CharecterImageNumber = CharecterImageNumber +1 ;
+    if(CharecterImageNumber == 11){
+        CharecterImageNumber = 1;
+    }
+    Robo.src = "assents/character/Idle%20(" + CharecterImageNumber + ").png";
+}
+
+function idleAnimationStart(){
+    AnimationNumber = setInterval(MoveAnimation,150);
+}
+
+
+
+
+// ------------------------------------------Run Animation--------------------------------------------------
+RunAnimationNumber=0;
+CharecterRunImageNumber=0;
+function RunAnimation(){
+    CharecterRunImageNumber = CharecterRunImageNumber +1 ;
+    if(CharecterRunImageNumber == 9){
+        CharecterRunImageNumber = 1;
+    }
+    Robo.src = "assents/character/Run%20(" + CharecterRunImageNumber + ").png";
+}
+
+function idleRunAnimationStart(){
+    RunAnimationNumber = setInterval(RunAnimation,150);
+}
+
+
+
+// ------------------------------------------dead Animation--------------------------------------------------
+
+DeadAnimationNumber=0;
+CharecterDeadImageNumber=0;
+function RunAnimation(){
+    CharecterDeadImageNumber = CharecterDeadImageNumber +1 ;
+    if(CharecterDeadImageNumber == 12){
+        CharecterDeadImageNumber = 1;
+    }
+    Robo.src = "assents/character/Dead%20(" + CharecterDeadImageNumber + ").png";
+}
+
+function idleDeadAnimationStart(){
+    DeadAnimationNumber = setInterval(RunAnimation,100);
+}
