fe49a5c8d917ab51a6101b00babfca1b4923d4df
Kdulanjan <kalananil19@gmail.com>
2022-11-25 20:06:06 +0530

    create moving enemy to right

--- assigment/ADI/2D space game/Enemy.js
+++ assigment/ADI/2D space game/Enemy.js
@@ -2,13 +2,18 @@ export  default class Enemy{
     constructor(x,y, imageNumber) {
         this.x=x;
         this.y=y;
-    this.width=45;
-    this.height=35;
+    this.width=50;
+    this.height=50;
 
         this.image =new Image();
         this.image.src=`assents/Spaceship${imageNumber}.png`;
     }
     draw(ctx){
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+    }
+    move(xVelocity, yVelocity){
+        this.x +=xVelocity;
+        this.y +=yVelocity;
+
     }
 }
